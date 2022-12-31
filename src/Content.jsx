4467f278037d406965aa789f48884719619ce8b5
DominikFrackowiak <rorschach@op.pl>
2022-12-31 10:01:57 +0100

    refactoring: useEffect hook used to save data

--- src/Content.jsx
+++ src/Content.jsx
@@ -5,43 +5,39 @@ import SearchItem from './SearchItem'
 
 export default function Content() {
 	const [items, setItems] = useState(
-		JSON.parse(localStorage.getItem('shoppingList'))
+		JSON.parse(localStorage.getItem('shoppingList')) || []
 	)
 
 	const [newItem, setNewItem] = useState('')
 
 	const [search, setSearch] = useState('')
 
-	// useEffect(() => {
-	// 	const storedItems = localStorage.getItem('shoppingList')
-	// 	if (storedItems) setItems(JSON.parse(storedItems))
-	// }, [])
-
-	const setAndSaveItems = itemsData => {
-		setItems(itemsData)
+	useEffect(() => {
 		localStorage.setItem('shoppingList', JSON.stringify(items))
-	}
+	}, [items])
+
+	// const setAndSaveItems = itemsData => {
+
+	// 	setItems(newItems)
+	// }
 
 	const handleCheck = id => {
 		const listItems = items.map(item =>
 			item.id === id ? { ...item, checked: !item.checked } : item
 		)
-		setAndSaveItems(listItems)
+		setItems(listItems)
 	}
 
 	const handleDelete = id => {
 		const filteredItems = items.filter(item => item.id !== id)
-		setAndSaveItems(filteredItems)
+		setItems(filteredItems)
 	}
 
 	const handleSubmit = e => {
 		e.preventDefault()
 		if (!newItem) return
 
-		setAndSaveItems([
-			...items,
-			{ id: Date.now(), checked: false, item: newItem },
-		])
+		setItems([...items, { id: Date.now(), checked: false, item: newItem }])
 		setNewItem('')
 	}
 
@@ -56,26 +52,30 @@ export default function Content() {
 
 			{items.length === 0 && <p>No items on the list</p>}
 			<ul>
-				{items.filter(item=>((item.item).toLowerCase()).includes(search.toLowerCase())).map(item => (
-					<li key={item.id} className='item'>
-						<input
-							type='checkbox'
-							checked={item.checked}
-							onChange={() => handleCheck(item.id)}
-						/>
-						<label
-							style={item.checked ? { textDecoration: 'line-through' } : null}
-							onDoubleClick={() => handleCheck(item.id)}
-						>
-							{item.item}
-						</label>
-						<FaTrashAlt
-							role='button'
-							tabIndex='0'
-							onClick={() => handleDelete(item.id)}
-						/>
-					</li>
-				))}
+				{items
+					.filter(item =>
+						item.item.toLowerCase().includes(search.toLowerCase())
+					)
+					.map(item => (
+						<li key={item.id} className='item'>
+							<input
+								type='checkbox'
+								checked={item.checked}
+								onChange={() => handleCheck(item.id)}
+							/>
+							<label
+								style={item.checked ? { textDecoration: 'line-through' } : null}
+								onDoubleClick={() => handleCheck(item.id)}
+							>
+								{item.item}
+							</label>
+							<FaTrashAlt
+								role='button'
+								tabIndex='0'
+								onClick={() => handleDelete(item.id)}
+							/>
+						</li>
+					))}
 			</ul>
 		</main>
 	)
